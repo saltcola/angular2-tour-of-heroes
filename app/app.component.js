d06ac77358118d77969be8051e41919c9872b1a7
Gang Fan <gang.fan@uconn.edu>
2016-07-11 15:27:44 -0400

    Making a Hero Detail Component

--- app/app.component.js
+++ app/app.component.js
@@ -9,12 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
-}());
-exports.Hero = Hero;
+var hero_detail_component_1 = require('./hero-detail.component');
 var HEROES = [
     { id: 11, name: 'Mr. Nice' },
     { id: 12, name: 'Narco' },
@@ -38,8 +33,9 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n\t\t  <li *ngFor=\"let hero of heroes\" \n\t\t  [class.selected]=\"hero === selectedHero\"\n\t\t  (click)=\"onSelect(hero)\">\n\t\t    <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t  </li>\n\t\t</ul>\n\t\t<div *ngIf=\"selectedHero\">\n\t\t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n\t\t\t<div>\n\t\t\t\t<label>name: </label>\n\t\t\t\t<input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\">\n\t\t\t</div>\n\t\t</div>\n\t\t",
-            styles: ["\n\t\t.selected {\n\t\t  background-color: #CFD8DC !important;\n\t\t  color: white;\n\t\t}\n\t\t.heroes {\n\t\t  margin: 0 0 2em 0;\n\t\t  list-style-type: none;\n\t\t  padding: 0;\n\t\t  width: 15em;\n\t\t}\n\t\t.heroes li {\n\t\t  cursor: pointer;\n\t\t  position: relative;\n\t\t  left: 0;\n\t\t  background-color: #EEE;\n\t\t  margin: .5em;\n\t\t  padding: .3em 0;\n\t\t  height: 1.6em;\n\t\t  border-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\t  background-color: #BBD8DC !important;\n\t\t  color: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\t  color: #607D8B;\n\t\t  background-color: #DDD;\n\t\t  left: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\t  position: relative;\n\t\t  top: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\t  display: inline-block;\n\t\t  font-size: small;\n\t\t  color: white;\n\t\t  padding: 0.8em 0.7em 0 0.7em;\n\t\t  background-color: #607D8B;\n\t\t  line-height: 1em;\n\t\t  position: relative;\n\t\t  left: -1px;\n\t\t  top: -4px;\n\t\t  height: 1.8em;\n\t\t  margin-right: .8em;\n\t\t  border-radius: 4px 0 0 4px;\n\t\t}\n\t\t"]
+            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n\t\t  <li *ngFor=\"let hero of heroes\" \n\t\t  [class.selected]=\"hero === selectedHero\"\n\t\t  (click)=\"onSelect(hero)\">\n\t\t    <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t  </li>\n\t\t</ul>\n\t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\t\t\n\t\t",
+            styles: ["\n\t\t.selected {\n\t\t  background-color: #CFD8DC !important;\n\t\t  color: white;\n\t\t}\n\t\t.heroes {\n\t\t  margin: 0 0 2em 0;\n\t\t  list-style-type: none;\n\t\t  padding: 0;\n\t\t  width: 15em;\n\t\t}\n\t\t.heroes li {\n\t\t  cursor: pointer;\n\t\t  position: relative;\n\t\t  left: 0;\n\t\t  background-color: #EEE;\n\t\t  margin: .5em;\n\t\t  padding: .3em 0;\n\t\t  height: 1.6em;\n\t\t  border-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\t  background-color: #BBD8DC !important;\n\t\t  color: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\t  color: #607D8B;\n\t\t  background-color: #DDD;\n\t\t  left: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\t  position: relative;\n\t\t  top: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\t  display: inline-block;\n\t\t  font-size: small;\n\t\t  color: white;\n\t\t  padding: 0.8em 0.7em 0 0.7em;\n\t\t  background-color: #607D8B;\n\t\t  line-height: 1em;\n\t\t  position: relative;\n\t\t  left: -1px;\n\t\t  top: -4px;\n\t\t  height: 1.8em;\n\t\t  margin-right: .8em;\n\t\t  border-radius: 4px 0 0 4px;\n\t\t}\n\t\t"],
+            directives: [hero_detail_component_1.HeroDetailComponent]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
