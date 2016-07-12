f6175ac3882756046d6d05ddfba14965d7d06572
Gang Fan <gang.fan@uconn.edu>
2016-07-11 21:26:28 -0400

    Services

--- app/app.component.js
+++ app/app.component.js
@@ -10,34 +10,29 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var hero_detail_component_1 = require('./hero-detail.component');
-var HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+var hero_service_1 = require('./hero.service');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
         this.title = 'Tour of Heroes';
-        this.heroes = HEROES;
     }
-    AppComponent.prototype.onSelect = function (hero) {
-        this.selectedHero = hero;
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
     };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n\t\t  <li *ngFor=\"let hero of heroes\" \n\t\t  [class.selected]=\"hero === selectedHero\"\n\t\t  (click)=\"onSelect(hero)\">\n\t\t    <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t  </li>\n\t\t</ul>\n\t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\t\t\n\t\t",
-            styles: ["\n\t\t.selected {\n\t\t  background-color: #CFD8DC !important;\n\t\t  color: white;\n\t\t}\n\t\t.heroes {\n\t\t  margin: 0 0 2em 0;\n\t\t  list-style-type: none;\n\t\t  padding: 0;\n\t\t  width: 15em;\n\t\t}\n\t\t.heroes li {\n\t\t  cursor: pointer;\n\t\t  position: relative;\n\t\t  left: 0;\n\t\t  background-color: #EEE;\n\t\t  margin: .5em;\n\t\t  padding: .3em 0;\n\t\t  height: 1.6em;\n\t\t  border-radius: 4px;\n\t\t}\n\t\t.heroes li.selected:hover {\n\t\t  background-color: #BBD8DC !important;\n\t\t  color: white;\n\t\t}\n\t\t.heroes li:hover {\n\t\t  color: #607D8B;\n\t\t  background-color: #DDD;\n\t\t  left: .1em;\n\t\t}\n\t\t.heroes .text {\n\t\t  position: relative;\n\t\t  top: -3px;\n\t\t}\n\t\t.heroes .badge {\n\t\t  display: inline-block;\n\t\t  font-size: small;\n\t\t  color: white;\n\t\t  padding: 0.8em 0.7em 0 0.7em;\n\t\t  background-color: #607D8B;\n\t\t  line-height: 1em;\n\t\t  position: relative;\n\t\t  left: -1px;\n\t\t  top: -4px;\n\t\t  height: 1.8em;\n\t\t  margin-right: .8em;\n\t\t  border-radius: 4px 0 0 4px;\n\t\t}\n\t\t"],
-            directives: [hero_detail_component_1.HeroDetailComponent]
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\"\n        [class.selected]=\"hero === selectedHero\"\n        (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  ",
+            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "],
+            directives: [hero_detail_component_1.HeroDetailComponent],
+            providers: [hero_service_1.HeroService]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [hero_service_1.HeroService])
     ], AppComponent);
     return AppComponent;
 }());
